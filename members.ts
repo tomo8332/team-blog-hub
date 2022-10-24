67fcccb69bdefc168e775d71cf54bc7ca930bd0b
tomo8332 <suzuki.tomoyuki.8332@gmail.com>
2022-10-24 16:13:36 +0900

    fix

--- members.ts
+++ members.ts
@@ -9,8 +9,7 @@ export const members: Member[] = [
       "デザインが好きなプログラマー。開発者向けの情報共有プラットフォームzenn.devを開発しています。",
     avatarSrc: "/avatars/catnose.jpg",
     sources: [
-      "https://zenn.dev/catnose99/feed",
-      "https://catnose.medium.com/feed",
+      "https://note.com/retail_ai/rss",
     ],
   },
 ];
