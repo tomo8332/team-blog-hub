ed3a237d38f3fe7a09955fd87b6fb650f3f9369e
tomo8332 <suzuki.tomoyuki.8332@gmail.com>
2022-10-24 16:27:05 +0900

    fix

--- members.ts
+++ members.ts
@@ -5,6 +5,7 @@ export const members: Member[] = [
     id: "catnose",
     name: "CatNose",
     role: "CTO",
+    avatarSrc: "/avatars/catnose.jpg",
     sources: [
       "https://note.com/retail_ai/rss",
     ],
