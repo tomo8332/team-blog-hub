15a58e8c4cd621ac826d4c09e7662ee3a1d3973a
tomo8332 <suzuki.tomoyuki.8332@gmail.com>
2022-10-24 16:18:21 +0900

    fix

--- members.ts
+++ members.ts
@@ -5,8 +5,6 @@ export const members: Member[] = [
     id: "catnose",
     name: "CatNose",
     role: "CTO",
-    bio:
-      "デザインが好きなプログラマー。開発者向けの情報共有プラットフォームzenn.devを開発しています。",
     avatarSrc: "/avatars/catnose.jpg",
     sources: [
       "https://note.com/retail_ai/rss",
